1173a6eac504c29e661d26492c0921161d217ff3
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2025-06-03 18:44:10 -0300

    feat: add clinic form page

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,31 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
+const ClinicFormPage = () => {
+  return (
+    <div>
+      <Dialog open={true}>
+        <form>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Associe-se a uma clínica</DialogTitle>
+              <DialogDescription>
+                Preencha o formulário abaixo para se associar a uma clínica.
+              </DialogDescription>
+            </DialogHeader>
+            <ClinicForm />
+          </DialogContent>
+        </form>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ClinicFormPage;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,30 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
 
+import { db } from "../db";
+import { usersToClinicsTable } from "../db/schema";
 import { SignOutButton } from "./components/sing-out-button";
 
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
-    headers: await headers(), 
+    headers: await headers(),
   });
 
   if (!session) {
     redirect("/authentication");
+  }
+
+  // Catch the user's clinic
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  // If the user doesn't have a clinic, redirect to the clinic form
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
 
   return (
